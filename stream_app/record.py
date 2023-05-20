1597afecba4cfe414e925c70d7181c2b6603c5bd
minusno0708 <naoyao1014@gmail.com>
2023-05-20 23:28:19 +0900

    機能統合

--- stream_app/record.py
+++ stream_app/record.py
@@ -5,8 +5,10 @@ from django.core.files.base import ContentFile
 from .models import Record
 
 def record_capture(image):
-    now = datetime.datetime.now()
-    image_name = str(time.mktime(now.timetuple()) * 1000) + "jpg"
+    dt_now = datetime.datetime.now()
+    now = dt_now.strftime("%Y-%m-%d _%H-%M-%S")
+    now += "-" + str(int(1360287003083988472 % 1000000000)).zfill(4)
+    image_name = now + ".jpg"
     record_image = Record()
     record_image.image.save(image_name,ContentFile(image))
     record_image.save()  
--- stream_app/views.py
+++ stream_app/views.py
@@ -1,18 +1,26 @@
+import time
+import datetime
+
 import cv2
-
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, StreamingHttpResponse
+from django.views import View
 
-from .record import record_capture
+from stream_app.detect.detect import detect
+from stream_app.record import record_capture
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<img src='/video_feed'/>")
+# ストリーミング画像・映像を表示するview
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'stream_app/index.html', {})
 
+# ストリーミング画像を定期的に返却するview
 def video_feed_view():
     video = generate_frame()
     return lambda _: StreamingHttpResponse(video, content_type='multipart/x-mixed-replace; boundary=frame')
 
+
+# フレーム生成・返却する処理
 def generate_frame():
     capture = cv2.VideoCapture(0)  # USBカメラから
 
@@ -20,21 +28,32 @@ def generate_frame():
         if not capture.isOpened():
             print("Capture is not opened.")
             break
-            
         # カメラからフレーム画像を取得
         ret, frame = capture.read()
         if not ret:
             print("Failed to read frame.")
             break
 
+        conf, frame = detect(frame)
+        frame = set_time(frame)
+
         # フレーム画像バイナリに変換
         ret, jpeg = cv2.imencode('.jpg', frame)
         byte_frame = jpeg.tobytes()
 
-        record_capture(byte_frame)
+        if conf > 0.75:
+            record_capture(byte_frame)
 
         # フレーム画像のバイナリデータをユーザーに送付する
         yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + byte_frame + b'\r\n\r\n')
-
+               b'Content-Type: image/jpeg\r\n\r\n' + byte_frame + b'\r\n\r\n')
+        
+        time.sleep(0.2)
     capture.release()
+
+def set_time(frame):
+    dt_now = datetime.datetime.now()
+    now = dt_now.strftime("%Y/%m/%d %H:%M:%S")
+    cv2.putText(frame, now, (0, 15), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 1, cv2.LINE_AA)
+
+    return frame
